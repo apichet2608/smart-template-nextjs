57c72151ec1c208581e4446d2996181935bc6bce
apichet <apichet.jtd@outlook.com>
2025-01-30 09:51:33 +0700

    Add initial layout and homepage components, configure Next.js settings, and update dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/smart-template",
+  assetPrefix: "/smart-template/",
+  output: "export",
+  distDir: "dist",
+  /* other config options here */
 };
 
 export default nextConfig;
---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+const Homepage = () => {
+  return (
+    <div className="flex flex-col ">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+    </div>
+  );
+};
+
+export default Homepage;
